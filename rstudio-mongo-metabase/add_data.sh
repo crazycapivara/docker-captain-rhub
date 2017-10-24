ae20780cbc4ffca5e851431af49e6290f905dbda
skuethe <stefan.kuethe@manserv.de>
2017-10-24 09:58:48 +0100

    Update add data scripts

--- rstudio-mongo-metabase/add_data.sh
+++ rstudio-mongo-metabase/add_data.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-docker run -it --rm --network  rstudiomongometabase_default rstudiomongometabase_rstudio r /home/rstudio/scripts/dl_stats.R
+#docker run -it --rm --network  rstudiomongometabase_default rstudiomongometabase_rstudio r /home/rstudio/scripts/dl_stats.R
+docker run -it --rm -v `pwd`/scripts:/scripts --network rstudiomongometabase_default rstudiomongometabase_rstudio r /scripts/dl_stats.R
 
--- rstudio-mongo-metabase/scripts/dl_stats.R
+++ rstudio-mongo-metabase/scripts/dl_stats.R
@@ -2,14 +2,17 @@ library(cranlogs)
 library(mongolite)
 
 # get logs
+cat("Fetch logs\n")
 pkgs <- c("shiny", "shinydashboard", "rmarkdown", "leaflet")
 logs <- cran_downloads(pkgs, when = "last-month")
+print(tail(logs))
 
 # write logs to db
 db <- "rstudio"
 collection <- "cranlogs"
 
+cat("insert data\n")
 conn <- mongo(collection, db = db, url="mongodb://mongodb")
 conn$count()
 conn$insert(logs)
-
+cat("done\n")
