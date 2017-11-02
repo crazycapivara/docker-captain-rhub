ad90272f50c976f9546d208b856f9863301b89e0
skuethe <stefan.kuethe@manserv.de>
2017-11-02 08:09:20 +0000

    Update gitlab runner config

--- gitlab-ci/run_runner.sh
+++ gitlab-ci/run_runner.sh
@@ -3,5 +3,5 @@ docker run -d --name gitlab-runner \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v `pwd`/gitlab-runner/config:/etc/gitlab-runner \
   gitlab/gitlab-runner:latest
-docker exec -it gitlab-runner gitlab-runner register
+#docker exec -it gitlab-runner gitlab-runner register
 
